2f3a9a4f868219462449ace3b9405b3e09d188bf
vango98 <greencolorapple@gmail.com>
2024-12-07 23:07:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 function goToPort(x){
     switch(x){
         case 0:
-            window.location.href = "https://vangoooo.dcism.org/ipr/";
+            window.location.href = "https://vangoooo.dcism.org/ipr.github.io/";
             break;
         case 1:
-            window.location.href = "https://vangoooo.dcism.org/ipr/pages/portfolio1.html";
+            window.location.href = "https://vangoooo.dcism.org/ipr.github.io/pages/portfolio1.html";
             break;
         case 2:
-            window.location.href = "https://vangoooo.dcism.org/ipr/pages/portfolio2.html";
+            window.location.href = "https://vangoooo.dcism.org/ipr.github.io/pages/portfolio2.html";
             break;
         case 3:
-            window.location.href = "https://vangoooo.dcism.org/ipr/pages/portfolio3.html";
+            window.location.href = "https://vangoooo.dcism.org/ipr.github.io/pages/portfolio3.html";
             break;
         case 4:
-            window.location.href = "https://vangoooo.dcism.org/ipr/pages/portfolio4.html";
+            window.location.href = "https://vangoooo.dcism.org/ipr.github.io/pages/portfolio4.html";
             break;
         case 5:
-            window.location.href = "https://vangoooo.dcism.org/ipr/pages/portfolio5.html";
+            window.location.href = "https://vangoooo.dcism.org/ipr.github.io/pages/portfolio5.html";
             break;
         case 6:
-            window.location.href = "https://vangoooo.dcism.org/ipr/pages/portfolio6.html";
+            window.location.href = "https://vangoooo.dcism.org/ipr.github.io/pages/portfolio6.html";
             break;
     }
 }
