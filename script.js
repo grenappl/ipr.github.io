269d6d2e276be1e774c858bb29e096e9686787ec
vango98 <greencolorapple@gmail.com>
2024-12-07 22:39:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function goToPort(x){
+    switch(x){
+        case 0:
+            window.location.href = "http://127.0.0.1:5500/Ruelan_IvanPaul-CIS1102/index.html";
+            break;
+        case 1:
+            window.location.href = "http://127.0.0.1:5500/Ruelan_IvanPaul-CIS1102/pages/portfolio1.html";
+            break;
+        case 2:
+            window.location.href = "http://127.0.0.1:5500/Ruelan_IvanPaul-CIS1102/pages/portfolio2.html";
+            break;
+        case 3:
+            window.location.href = "http://127.0.0.1:5500/Ruelan_IvanPaul-CIS1102/pages/portfolio3.html";
+            break;
+        case 4:
+            window.location.href = "http://127.0.0.1:5500/Ruelan_IvanPaul-CIS1102/pages/portfolio4.html";
+            break;
+        case 5:
+            window.location.href = "http://127.0.0.1:5500/Ruelan_IvanPaul-CIS1102/pages/portfolio5.html";
+            break;
+        case 6:
+            window.location.href = "http://127.0.0.1:5500/Ruelan_IvanPaul-CIS1102/pages/portfolio6.html";
+            break;
+    }
+}
